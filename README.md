85693d760077eb162f1cfd97f3eeedccaa44ef58
kh <kevin.hirczy@gmail.com>
2024-06-23 23:09:27 +0200

    feat: add next auth with prisma and sqlite

--- README.md
+++ README.md
@@ -1,36 +1,15 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Authentication for Next.js with NextAuth
 
-## Getting Started
+Source code for [How to add Authentication for Next.js with NextAuth 4](https://nehalist.io/authentication-for-nextjs-with-nextauth-part-1).
 
-First, run the development server:
+## Usage
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+1. Clone the repository
+2. Install dependencies with `npm ci`
+3. Start docker containers (for mailpit and postgres) with `docker-compose up -d`
+4. Copy `.env.` to `.env.local` and adjust the values
+5. Run the development server with `npm run dev`
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Author
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+[nehalist.io](https://nehalist.io)
---
+++ src/lib/auth.ts
@@ -0,0 +1,106 @@
+import prisma from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { compare, genSalt, hash } from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import CredentialsProvider from "next-auth/providers/credentials";
+import EmailProvider from "next-auth/providers/email";
+import GitHubProvider from "next-auth/providers/github";
+import { cookies } from "next/headers";
+
+async function hashAndSaltPassword(password: string, saltRounds = 10) {
+  const salt = await genSalt(saltRounds);
+  return hash(password, salt);
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter, // this cast is required to prevent type errors, see https://github.com/nextauthjs/next-auth/issues/6106
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        let user = await prisma.user.findFirst({
+          where: {
+            name: credentials.username,
+          },
+        });
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              name: credentials.username,
+              password: await hashAndSaltPassword(credentials.password),
+            },
+          });
+        }
+
+        if (!user.password) {
+          return null;
+        }
+
+        const comparison = await compare(credentials.password, user.password);
+        if (comparison) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      // We only want to handle this for credentials provider
+      if (account?.provider !== "credentials") {
+        return true;
+      }
+
+      // Our session/cookie settings
+      const tokenName =
+        process.env.NODE_ENV === "development"
+          ? "next-auth.session-token"
+          : "__Secure-next-auth.session-token";
+      const expireAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+      const token = require("crypto").randomBytes(32).toString("hex");
+
+      // Create a session in our database
+      await prisma.session.create({
+        data: {
+          sessionToken: token,
+          userId: user.id,
+          expires: expireAt,
+        },
+      });
+
+      // Set our cookie
+      cookies().set(tokenName, token, {
+        expires: expireAt,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        path: "/",
+      });
+
+      // Return to "/" after sign in
+      return "/";
+    },
+  },
+  debug: process.env.NODE_ENV === "development",
+};
